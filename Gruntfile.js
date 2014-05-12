7a66cc005f3081fd0b1dddaabce3097c9019f949
Ryan Murphy <github@accounts.rdmurphy.org>
2014-05-12 17:42:30 -0500

    Initial commit II, more setup

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          './<%= pkg.name %>/static/css/app.css': './<%= pkg.name %>/static/scss/app.scss'
+        }
+      }
+    },
+
+    watch: {
+      sass: {
+        files: ['<%= pkg.name %>/static/scss/**/*.scss'],
+        tasks: ['sass']
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('dev', ['default', 'watch']);
+};
