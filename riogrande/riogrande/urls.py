94bb45b82168eb6550baae927fe202580cf446da
Ryan Murphy <github@accounts.rdmurphy.org>
2014-05-26 13:36:19 -0500

    Added support for finding uploaded media files for dev

--- riogrande/riogrande/urls.py
+++ riogrande/riogrande/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,4 +12,5 @@ urlpatterns = patterns(
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
