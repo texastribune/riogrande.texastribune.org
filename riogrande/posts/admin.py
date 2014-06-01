04e449dbb17849b7ec64f4d3225814fcab0346ff
Ryan Murphy <github@accounts.rdmurphy.org>
2014-06-01 16:43:15 -0500

    Trigger the loading of ckeditor for the Post admin

--- riogrande/posts/admin.py
+++ riogrande/posts/admin.py
@@ -1,10 +1,21 @@
+from django import forms
 from django.contrib import admin
+from django.db import models
 
-from . import models
+from .models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
-    pass
+    formfield_overrides = {
+        models.TextField: {
+            'widget': forms.Textarea(attrs={
+                'class':'ckeditor',
+            })
+        }
+    }
+
+    class Media:
+        js = ('ckeditor/ckeditor.js', )
 
 
-admin.site.register(models.Post, PostAdmin)
+admin.site.register(Post, PostAdmin)
