8ab622e0bede3cb54762ea25b78848c7e8aa8598
Ryan Murphy <github@accounts.rdmurphy.org>
2014-06-15 12:49:42 -0500

    Made Post play nice with Day pub_date

--- riogrande/posts/models.py
+++ riogrande/posts/models.py
@@ -5,11 +5,12 @@ from photos.models import Photo
 from riogrande.choices import PublicationStatus
 from riogrande.managers import PublishedObjectsManager
 
+from days.models import Day
 
 class Post(models.Model):
 
     # publication fields
-    pub_date = models.DateTimeField()
+    pub_date = models.OneToOneField(Day, related_name='post_for')
     pub_status = models.CharField(max_length=1,
                                   choices=PublicationStatus.choices,
                                   default=PublicationStatus.Draft)
