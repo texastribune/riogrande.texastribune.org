3f3bdab6d410e500d5b98852753869744554b86a
Ryan Murphy <github@accounts.rdmurphy.org>
2014-06-15 12:52:17 -0500

    The Photo model needs to keep using the old pub_date, so it needs a its own manager

--- riogrande/riogrande/managers.py
+++ riogrande/riogrande/managers.py
@@ -14,6 +14,19 @@ class PublishedObjectsManager(models.Manager):
             .filter(
                 pub_status=PublicationStatus.Published,
                 # add 1 second of slop for safety
+                pub_date__date__lte=now() + timedelta(seconds=1)
+            )
+        )
+
+
+class PublishedObjectsPhotoManager(models.Manager):
+    def get_query_set(self):
+        return (
+            super(PublishedObjectsPhotoManager, self)
+            .get_query_set()
+            .filter(
+                pub_status=PublicationStatus.Published,
+                # add 1 second of slop for safety
                 pub_date__lte=now() + timedelta(seconds=1)
             )
         )
