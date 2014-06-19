a4057b99dc2e2821f5b0c2ef8e7874d73f431c0b
Ryan Murphy <github@accounts.rdmurphy.org>
2014-06-19 18:59:50 -0500

    Blow way Pings migrations too

--- riogrande/pings/migrations/0001_initial.py
+++ riogrande/pings/migrations/0001_initial.py
@@ -12,7 +12,9 @@ class Migration(SchemaMigration):
         db.create_table(u'pings_ping', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('location', self.gf('django.contrib.gis.db.models.fields.PointField')()),
-            ('pub_date', self.gf('django.db.models.fields.DateTimeField')()),
+            ('pub_status', self.gf('django.db.models.fields.CharField')(default='P', max_length=1)),
+            ('pub_date', self.gf('django.db.models.fields.related.ForeignKey')(related_name='pings', null=True, to=orm['days.Day'])),
+            ('pub_time', self.gf('django.db.models.fields.TimeField')()),
             ('api_id', self.gf('django.db.models.fields.PositiveIntegerField')(unique=True)),
         ))
         db.send_create_signal(u'pings', ['Ping'])
@@ -24,12 +26,19 @@ class Migration(SchemaMigration):
 
 
     models = {
+        u'days.day': {
+            'Meta': {'ordering': "('-date',)", 'object_name': 'Day'},
+            'date': ('django.db.models.fields.DateField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
         u'pings.ping': {
             'Meta': {'object_name': 'Ping'},
             'api_id': ('django.db.models.fields.PositiveIntegerField', [], {'unique': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'location': ('django.contrib.gis.db.models.fields.PointField', [], {}),
-            'pub_date': ('django.db.models.fields.DateTimeField', [], {})
+            'pub_date': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'pings'", 'null': 'True', 'to': u"orm['days.Day']"}),
+            'pub_status': ('django.db.models.fields.CharField', [], {'default': "'P'", 'max_length': '1'}),
+            'pub_time': ('django.db.models.fields.TimeField', [], {})
         }
     }
 
