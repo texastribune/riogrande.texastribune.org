c951139e2ac09daab6a41b8c65b2220152a81217
Ryan Murphy <github@accounts.rdmurphy.org>
2014-06-19 19:03:23 -0500

    Away with Post migrations

--- riogrande/posts/migrations/0001_initial.py
+++ riogrande/posts/migrations/0001_initial.py
@@ -11,11 +11,13 @@ class Migration(SchemaMigration):
         # Adding model 'Post'
         db.create_table(u'posts_post', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('pub_date', self.gf('django.db.models.fields.DateTimeField')()),
+            ('pub_date', self.gf('django.db.models.fields.related.OneToOneField')(related_name='post_for', unique=True, to=orm['days.Day'])),
+            ('pub_status', self.gf('django.db.models.fields.CharField')(default='D', max_length=1)),
             ('headline', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('slug', self.gf('django.db.models.fields.SlugField')(max_length=100)),
             ('text', self.gf('django.db.models.fields.TextField')()),
             ('lede_art', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['photos.Photo'])),
+            ('location', self.gf('django.contrib.gis.db.models.fields.PointField')()),
         ))
         db.send_create_signal(u'posts', ['Post'])
 
@@ -26,14 +28,19 @@ class Migration(SchemaMigration):
 
 
     models = {
+        u'days.day': {
+            'Meta': {'ordering': "('-date',)", 'object_name': 'Day'},
+            'date': ('django.db.models.fields.DateField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
         u'photos.photo': {
-            'Meta': {'ordering': "['-date_added']", 'object_name': 'Photo'},
+            'Meta': {'ordering': "['-pub_date']", 'object_name': 'Photo'},
             'caption': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'credit': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
-            'date_added': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
-            'is_public': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'pub_status': ('django.db.models.fields.CharField', [], {'default': "'D'", 'max_length': '1'}),
             'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
@@ -42,7 +49,9 @@ class Migration(SchemaMigration):
             'headline': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'lede_art': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['photos.Photo']"}),
-            'pub_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'location': ('django.contrib.gis.db.models.fields.PointField', [], {}),
+            'pub_date': ('django.db.models.fields.related.OneToOneField', [], {'related_name': "'post_for'", 'unique': 'True', 'to': u"orm['days.Day']"}),
+            'pub_status': ('django.db.models.fields.CharField', [], {'default': "'D'", 'max_length': '1'}),
             'slug': ('django.db.models.fields.SlugField', [], {'max_length': '100'}),
             'text': ('django.db.models.fields.TextField', [], {})
         }
